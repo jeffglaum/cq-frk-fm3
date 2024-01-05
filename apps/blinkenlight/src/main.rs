a3fab5956bd10df60a10a8426c2a97b626e15454
Jeff Glaum <jeffglaum@live.com>
2024-01-05 12:50:07 -0800

    Added the actual blinking part...

--- apps/blinkenlight/src/main.rs
+++ apps/blinkenlight/src/main.rs
@@ -3,11 +3,32 @@
 
 use panic_halt as _;
 
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry, exception};
+use cortex_m::peripheral::{syst, Peripherals};
 use mb9bf61xt;
 
-#[entry]
-fn main() -> ! {
+// SysTick timer interval (in ms).
+const SYSTICK_PERIOD: u32 = 1_000;
+
+// SysTick exception handler.
+#[exception]
+fn SysTick() {
+
+    let p = unsafe { mb9bf61xt::Peripherals::steal() };
+    let gpio = p.GPIO;
+
+    // Invert the output.
+    let current_pf3_val  = gpio.pdorf().read().pf3().bit_is_set();
+    gpio.pdorf().write(|w| {w.pf3().bit(! current_pf3_val)});
+
+    // Reset the SysTick timer.
+    let cp = unsafe { Peripherals::steal() };
+    let mut systick = cp.SYST;
+
+    systick.set_reload(SYSTICK_PERIOD);
+}
+
+fn initialize_gpio() {
 
     let p = unsafe { mb9bf61xt::Peripherals::steal() };
     let gpio = p.GPIO;
@@ -23,6 +44,28 @@ fn main() -> ! {
 
     // Set to low level.
     gpio.pdorf().write(|w| {w.pf3().clear_bit()});
+}
+
+fn initialize_systick() {
+
+    let cp = unsafe { Peripherals::steal() };
+    let mut systick = cp.SYST;
+
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(SYSTICK_PERIOD);
+    systick.clear_current();
+    systick.enable_interrupt();
+    systick.enable_counter();
+}
+
+#[entry]
+fn main() -> ! {
+
+    // Initialize the GPIO driving the external LED.
+    initialize_gpio();
+
+    // Initialize SysTick.
+    initialize_systick();
 
     loop {}
 }
